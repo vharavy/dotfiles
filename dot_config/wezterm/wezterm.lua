0e2fc7c7a00ecff2c4eb31a9bc83b346c476ceb8
Vitali Haravy <humaneprogrammer@gmail.com>
2023-08-26 20:46:36 +0100

    Initial commit

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.font = wezterm.font("IosevkaTerm Nerd Font")
+-- config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+config.font_size = 24
+config.color_scheme = "Solarized (light) (terminal.sexy)"
+
+return config
