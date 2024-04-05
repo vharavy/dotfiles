ba1f3e6534d5011c57c86b49ce0fa22d578864be
Vitali Haravy <humaneprogrammer@gmail.com>
2024-04-05 02:55:15 +0100

    Configure Neovim options

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,40 @@
+local opt = vim.opt
+
+-- Tab / Indentation
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.expandtab = true
+opt.smartindent = true
+opt.wrap = false
+
+-- Search
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+
+-- Appearance
+opt.number = true
+opt.termguicolors = true
+opt.colorcolumn = "100"
+opt.signcolumn = "yes"
+opt.cmdheight = 1
+opt.scrolloff = 10
+opt.completeopt = "menuone,noinsert,noselect"
+
+-- Behaviour
+opt.hidden = true
+opt.errorbells = false
+opt.swapfile = false
+opt.backup = false
+opt.undodir = vim.fn.expand("~/.nvim/undo")
+opt.undofile = true
+opt.backspace = "indent,eol,start"
+opt.splitright = true
+opt.splitbelow = true
+opt.autochdir = false
+opt.mouse:append("a")
+opt.clipboard:append("unnamedplus")
+opt.modifiable = true
+opt.encoding = "UTF-8"
