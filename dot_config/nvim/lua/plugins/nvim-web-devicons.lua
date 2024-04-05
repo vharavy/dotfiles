67030a1f6b3924d72e76ba5656c0b1249cbd0a1e
Vitali Haravy <humaneprogrammer@gmail.com>
2024-04-05 02:55:15 +0100

    Configure more Neovim plugins

---
+++ dot_config/nvim/lua/plugins/nvim-web-devicons.lua
@@ -0,0 +1,6 @@
+return {
+    "nvim-tree/nvim-web-devicons",
+    dependencies = {
+        "nvim-tree/nvim-tree.lua",
+    },
+}
--- dot_config/nvim/lua/plugins/solarized.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'shaunsingh/solarized.nvim',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd('colorscheme solarized')
-  end,
-}
