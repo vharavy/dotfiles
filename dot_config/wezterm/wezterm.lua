8994974ac7c729757e5ff0c6ce10d256b8562938
Vitali Haravy <humaneprogrammer@gmail.com>
2024-10-01 01:19:58 +0100

    Remove configs for Alacritty and WezTerm

--- dot_config/wezterm/wezterm.lua
+++
@@ -1,8 +0,0 @@
-local wezterm = require("wezterm")
-local config = {}
-
-config.font = wezterm.font("IosevkaTerm Nerd Font")
-config.font_size = 24
-config.color_scheme = "Solarized (light) (terminal.sexy)"
-
-return config
