4eff0f07b8707a4b1edb022ed45b19c9e6ff78ab
Vitali Haravy <humaneprogrammer@gmail.com>
2024-12-08 23:03:34 +0000

    Reformat Lua code and configure Telescope

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -21,7 +21,7 @@ vim.opt.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-    vim.opt.clipboard = 'unnamedplus'
+  vim.opt.clipboard = 'unnamedplus'
 end)
 
 -- Enable break indent
@@ -104,88 +104,286 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-    desc = 'Highlight when yanking (copying) text',
-    group = vim.api.nvim_create_augroup('vitali-highlight-yank', { clear = true }),
-    callback = function()
-	vim.highlight.on_yank({ timeout = 300 })
-    end,
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('vitali-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank { timeout = 300 }
+  end,
 })
 
 -- [[ Lazy & Plugins ]]
 
 -- Boostrap Lazy
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-	vim.api.nvim_echo({
-	    { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-	    { out, "WarningMsg" },
-	    { "\nPress any key to exit..." },
-	}, true, {})
-	vim.fn.getchar()
-	os.exit(1)
-    end
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Configure Lazy
-require("lazy").setup({
-    spec = {
-	{
-	    "catppuccin/nvim",
-	    name = "catppuccin",
-	    priority = 1000,
-	    config = function()
-		vim.cmd.colorscheme "catppuccin-latte"
-	    end
-	},
-	-- TreeSitter
-	{
-	    "nvim-treesitter/nvim-treesitter",
-	    name = "treesitter",
-	    build = ":TSUpdate",
-	    config = function ()
-		local configs = require("nvim-treesitter.configs")
-		configs.setup({
-		    ensure_installed = { "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "zig" },
-		    modules = {},
-		    auto_install = false,
-		    sync_install = false,
-		    ignore_install = {},
-		    highlight = { enable = true },
-		    indent = { enable = true },
-		})
-	    end
-	},
-	-- LSP
-	{
-	    'neovim/nvim-lspconfig',
-	    dependencies = {
-		{
-		    -- Configures Lua LSP for your Neovim config, runtime and plugins
-		    -- used for completion, annotations and signatures of Neovim APIs
-		    'folke/lazydev.nvim',
-		    ft = 'lua',
-		    opts = {
-			library = {
-			    -- Load luvit types when the `vim.uv` word is found
-			    { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-			},
-		    },
-		},
-	    },
-	    config = function()
-		local lspconfig = require('lspconfig')
-		lspconfig.lua_ls.setup{}
-		lspconfig.zls.setup{}
-	    end,
-	},
+require('lazy').setup {
+  spec = {
+    {
+      'catppuccin/nvim',
+      name = 'catppuccin',
+      priority = 1000,
+      config = function()
+        vim.cmd.colorscheme 'catppuccin-latte'
+      end,
     },
-    -- Configure any other settings here. See the documentation for more details.
-    -- Color scheme that will be used when installing plugins.
-    install = { colorscheme = { "catppuccin-latte" } },
-    -- Automatically check for plugin updates.
-    checker = { enabled = false },
-})
+    -- TreeSitter
+    {
+      'nvim-treesitter/nvim-treesitter',
+      name = 'treesitter',
+      build = ':TSUpdate',
+      config = function()
+        local configs = require 'nvim-treesitter.configs'
+        configs.setup {
+          ensure_installed = { 'lua', 'vim', 'vimdoc', 'query', 'markdown', 'markdown_inline', 'zig' },
+          modules = {},
+          auto_install = false,
+          sync_install = false,
+          ignore_install = {},
+          highlight = { enable = true },
+          indent = { enable = true },
+        }
+      end,
+    },
+    -- Telescope
+    {
+      'nvim-telescope/telescope.nvim',
+      event = 'VimEnter',
+      branch = '0.1.x',
+      dependencies = {
+        { 'nvim-lua/plenary.nvim' },
+        { -- If encountering errors, see telescope-fzf-native README for installation instructions
+          'nvim-telescope/telescope-fzf-native.nvim',
+
+          -- `build` is used to run some command when the plugin is installed/updated.
+          -- This is only run then, not every time Neovim starts up.
+          build = 'make',
+
+          -- `cond` is a condition used to determine whether this plugin should be
+          -- installed and loaded.
+          cond = function()
+            return vim.fn.executable 'make' == 1
+          end,
+        },
+        { 'nvim-telescope/telescope-ui-select.nvim' },
+        { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      },
+      config = function()
+        -- See `:help telescope` and `:help telescope.setup()`
+        require('telescope').setup {
+          -- defaults = {
+          --   mappings = {
+          --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          --   },
+          -- },
+          -- pickers = {}
+          extensions = {
+            ['ui-select'] = {
+              require('telescope.themes').get_dropdown(),
+            },
+          },
+        }
+
+        -- Enable Telescope extensions if they are installed
+        pcall(require('telescope').load_extension, 'fzf')
+        pcall(require('telescope').load_extension, 'ui-select')
+
+        -- See `:help telescope.builtin`
+        local builtin = require 'telescope.builtin'
+        vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+        vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+        vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+        vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+        vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+        vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+        vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+        vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+        vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+        vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+        -- Slightly advanced example of overriding default behavior and theme
+        vim.keymap.set('n', '<leader>/', function()
+          -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+          builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+            winblend = 10,
+            previewer = false,
+          })
+        end, { desc = '[/] Fuzzily search in current buffer' })
+
+        -- It's also possible to pass additional configuration options.
+        --  See `:help telescope.builtin.live_grep()` for information about particular keys
+        vim.keymap.set('n', '<leader>s/', function()
+          builtin.live_grep {
+            grep_open_files = true,
+            prompt_title = 'Live Grep in Open Files',
+          }
+        end, { desc = '[S]earch [/] in Open Files' })
+      end,
+    },
+    -- LSP
+    {
+      'neovim/nvim-lspconfig',
+      dependencies = {
+        {
+          -- Configures Lua LSP for your Neovim config, runtime and plugins
+          -- used for completion, annotations and signatures of Neovim APIs
+          'folke/lazydev.nvim',
+          ft = 'lua',
+          opts = {
+            library = {
+              -- Load luvit types when the `vim.uv` word is found
+              { path = '{3rd}/luv/library', words = { 'vim%.uv' } },
+            },
+          },
+        },
+      },
+      config = function()
+        vim.api.nvim_create_autocmd('LspAttach', {
+          group = vim.api.nvim_create_augroup('vitali-lsp-attach', { clear = true }),
+          callback = function(event)
+            local map = function(keys, func, desc, mode)
+              mode = mode or 'n'
+              vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+            end
+
+            -- Jump to the definition of the word under your cursor.
+            --  This is where a variable was first declared, or where a function is defined, etc.
+            --  To jump back, press <C-t>.
+            map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
+            -- Find references for the word under your cursor.
+            map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
+            -- Jump to the implementation of the word under your cursor.
+            --  Useful when your language has ways of declaring types without an actual implementation.
+            map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+
+            -- Jump to the type of the word under your cursor.
+            --  Useful when you're not sure what type a variable is and you want to see
+            --  the definition of its *type*, not where it was *defined*.
+            map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+
+            -- Fuzzy find all the symbols in your current document.
+            --  Symbols are things like variables, functions, types, etc.
+            map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+
+            -- Fuzzy find all the symbols in your current workspace.
+            --  Similar to document symbols, except searches over your entire project.
+            map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+            -- Rename the variable under your cursor.
+            --  Most Language Servers support renaming across files, etc.
+            map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+
+            -- Execute a code action, usually your cursor needs to be on top of an error
+            -- or a suggestion from your LSP for this to activate.
+            map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+
+            -- WARN: This is not Goto Definition, this is Goto Declaration.
+            --  For example, in C this would take you to the header.
+            map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+            local client = vim.lsp.get_client_by_id(event.data.client_id)
+            if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+              local highlight_augroup = vim.api.nvim_create_augroup('vitali-lsp-highlight', { clear = false })
+              vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+                buffer = event.buf,
+                group = highlight_augroup,
+                callback = vim.lsp.buf.document_highlight,
+              })
+
+              vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+                buffer = event.buf,
+                group = highlight_augroup,
+                callback = vim.lsp.buf.clear_references,
+              })
+
+              vim.api.nvim_create_autocmd('LspDetach', {
+                group = vim.api.nvim_create_augroup('vitali-lsp-detach', { clear = true }),
+                callback = function(event2)
+                  vim.lsp.buf.clear_references()
+                  vim.api.nvim_clear_autocmds { group = 'vitali-lsp-highlight', buffer = event2.buf }
+                end,
+              })
+            end
+
+            -- Create a keymap to toggle inlay hints in the code, if the language server supports them.
+            if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+              map('<leader>th', function()
+                vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+              end, '[T]oggle Inlay [H]ints')
+            end
+          end,
+        })
+
+        local lspconfig = require 'lspconfig'
+        lspconfig.lua_ls.setup {}
+        lspconfig.zls.setup {}
+      end,
+    },
+    -- Formatting
+    {
+      'stevearc/conform.nvim',
+      event = { 'BufWritePre' },
+      cmd = { 'ConformInfo' },
+      keys = {
+        {
+          '<leader>f',
+          function()
+            require('conform').format { async = true, lsp_format = 'fallback' }
+          end,
+          mode = '',
+          desc = '[F]ormat buffer',
+        },
+      },
+      opts = {
+        notify_on_error = false,
+        format_on_save = function(bufnr)
+          -- Disable "format_on_save lsp_fallback" for languages that don't
+          -- have a well standardized coding style. You can add additional
+          -- languages here or re-enable it for the disabled ones.
+          local disable_filetypes = { c = true, cpp = true }
+          local lsp_format_opt
+          if disable_filetypes[vim.bo[bufnr].filetype] then
+            lsp_format_opt = 'never'
+          else
+            lsp_format_opt = 'fallback'
+          end
+          return {
+            timeout_ms = 500,
+            lsp_format = lsp_format_opt,
+          }
+        end,
+        formatters_by_ft = {
+          lua = { 'stylua' },
+          nix = { 'nixfmt' },
+          zig = { 'zigfmt' },
+          -- Conform can also run multiple formatters sequentially
+          -- python = { "isort", "black" },
+          --
+          -- You can use 'stop_after_first' to run the first available formatter from the list
+          -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        },
+      },
+    },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- Color scheme that will be used when installing plugins.
+  install = { colorscheme = { 'catppuccin-latte' } },
+  -- Automatically check for plugin updates.
+  checker = { enabled = false },
+}
